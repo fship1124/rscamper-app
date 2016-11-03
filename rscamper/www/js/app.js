573f22393adf6853e8fd74d8032af05266cb6e57
youngeun <0eunee0@gmail.com>
2016-11-03 14:32:11 +0900

    수정

--- rscamper/www/js/app.js
+++ rscamper/www/js/app.js
@@ -100,7 +100,6 @@ var app = angular.module('App', ['ionic', 'ionic-material', 'firebase', 'ngCordo
         }
       }
     })
-<<<<<<< HEAD
     // 로그인 메인 화면
     .state('app.login', {
       url: '/login',
@@ -151,7 +150,6 @@ var app = angular.module('App', ['ionic', 'ionic-material', 'firebase', 'ngCordo
         }
       }
     })
-=======
     .state('app.tourInfo', {
       cache: false,
       url: '/tourInfo',
@@ -172,58 +170,6 @@ var app = angular.module('App', ['ionic', 'ionic-material', 'firebase', 'ngCordo
         }
       }
     });
-
-
->>>>>>> ye
-
-
-  /*// setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });*/
 
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/main/main');
